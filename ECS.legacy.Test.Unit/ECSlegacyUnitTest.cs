ca36d919ef97ce8cec35627575e3e067aac37734
Rasmus Westland Knudsen <rasmuswestlandknudsen@hotmail.com>
2018-02-15 12:34:18 +0100

    added test project

---
+++ ECS.legacy.Test.Unit/ECSlegacyUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECS.Legacy;
+using NUnit.Framework;
+
+namespace ECS.legacy.Test.Unit
+{
+    [TestFixture]
+    public class ECSlegacyUnitTest
+    {
+        private ECS _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new ECS();
+        }
+    }
+}
